04958366056f1e0d57afbd32bee790b4bb4c0b05
UsernameAnda <93479145+kikisafera@users.noreply.github.com>
2022-06-09 19:44:15 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>SIB</b> NF COMPUTER
+    </div>
+    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+    reserved.
+  </footer>
